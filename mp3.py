b83c8e89c1a206591228434829f677a79b8fe0ed
Mohammedtalhas <36769231+Mohammedtalhas@users.noreply.github.com>
2019-01-02 18:33:40 +0530

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -1,9 +1,9 @@
 from gtts import gTTS
 import os
 import csv
-path = 'D:/DATA/projc'
+path = ''# mention ur path of the text file
 for filename in os.listdir(path):
-    if filename == 'mp3.txt':
+    if filename == 'mp3.txt': #place the name of the file as I've just given an example 
         x = open(filename)
         tts = gTTS(text=x.read(), lang='en')
         tts.save("good.mp3")
